9b6a6076e355d505059fdbd8c23ba81f660fe1f6
nikita <nikita.n.kapustin@gmail.com>
2023-04-29 17:06:44 +0300

    Solution 2.1 work_with_database added

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    price = models.FloatField()
+    image = models.TextField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Phone, self).save(*args, **kwargs)
