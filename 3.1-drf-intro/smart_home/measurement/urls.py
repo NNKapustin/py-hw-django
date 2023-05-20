8b1fe32e34c7c0aa782fbb6062f9874e37d05190
nikita <nikita.n.kapustin@gmail.com>
2023-05-20 14:22:06 +0300

    Solution 3.1 added, have problem with last GET request

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from .views import SensorCreateView, SensorDetailView, MeasurementCreateView
+
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorCreateView.as_view(), name='create_sensor'), 
+    path('sensors/<int:pk>/', SensorDetailView.as_view(), name='sensor'),
+    path('measurements/', MeasurementCreateView.as_view(), name='measurement')
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,19 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from .models import Sensor, Measurement
+from .serializers import SensorDetailSerializer, MeasurementSerializer
+
+
+class SensorCreateView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+class SensorDetailView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+class MeasurementCreateView(ListCreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
