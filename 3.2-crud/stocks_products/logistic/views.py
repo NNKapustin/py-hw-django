27255285799dd07ed8a45b3c97dab939e58c04a0
Nikita <40544749+NNKapustin@users.noreply.github.com>
2023-05-26 09:02:35 +0300

    Update views.py class StockViewSet changed

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,6 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
-from django.db.models import Q
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -15,17 +14,7 @@ class ProductViewSet(ModelViewSet):
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-
-    def get_queryset(self):
-        product_id = self.request.query_params.get('products')
-        search_query = self.request.query_params.get('search')
-        queryset = Stock.objects.all()
-        if product_id:
-            queryset = queryset.filter(positions__product_id=product_id)
-        elif search_query:
-            queryset = queryset.filter(
-                Q(positions__product__title__icontains=search_query) | 
-                Q(positions__product__description__icontains=search_query)
-                )
-        return queryset
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['products']
+    search_fields = ['products__title', 'products__description']
     
