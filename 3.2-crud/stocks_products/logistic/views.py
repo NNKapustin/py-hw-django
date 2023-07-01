5e4009c2d06eba9a9e07b2a164597c0da05142e5
Nikita <40544749+NNKapustin@users.noreply.github.com>
2023-07-01 18:44:25 +0300

    Update views.py

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
-from logistic.models import Product, Stock
-from logistic.serializers import ProductSerializer, StockSerializer
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
+
 
 
 class ProductViewSet(ModelViewSet):
